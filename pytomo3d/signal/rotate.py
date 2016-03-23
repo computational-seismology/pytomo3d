4baff69c449e9bba17d9e5052636cac75e8a4e06
lei <lei@princeton.edu>
2016-03-22 22:28:09 -0400

    add tests for rotating methods

--- pytomo3d/signal/rotate.py
+++ pytomo3d/signal/rotate.py
@@ -10,16 +10,28 @@ Methods that handles rotation of seismograms
 """
 from __future__ import print_function
 from obspy.core.util.geodetics import gps2DistAzimuth
-from math import pi, cos, sin
+from math import cos, sin
 from obspy import Stream
+import numpy as np
+from numpy import deg2rad
 
 
 SMALL_DEGREE = 0.01
 
 
+def calculate_baz(elat, elon, slat, slon):
+    _, _, baz = gps2DistAzimuth(elat, elon, slat, slon)
+    return baz
+
+
 def check_orthogonality(azim1, azim2):
     """
-    Check if two azimuth are orthogonal
+    Check if two azimuth are orthogonal, check whether
+    (azim1, azim2, vertical) forms a left-hand or right-hand
+    coordinate system.
+    Remember the defination of azimuth is angle between north
+    direction.
+    Unit is degree, not radian.
     """
     azim1 = azim1 % 360
     azim2 = azim2 % 360
@@ -42,7 +54,7 @@ def check_orthogonality(azim1, azim2):
             return False
 
 
-def rotate_certain_angle(d1, d2, angle):
+def rotate_certain_angle(d1, d2, angle, unit="degree"):
     """
     Basic rotating function.
 
@@ -62,6 +74,14 @@ def rotate_certain_angle(d1, d2, angle):
     :param angle: component azimuth of data2
     :return: two new components after rotation
     """
+    if unit == "degree":
+        angle = deg2rad(angle)
+    elif unit == "radian":
+        angle = angle
+    else:
+        raise ValueError("Unregonized unit(%s): 1) degree; 2) radian"
+                         % unit)
+
     dnew1 = d1 * cos(angle) + d2 * sin(angle)
     dnew2 = -d1 * sin(angle) + d2 * cos(angle)
     return dnew1, dnew2
@@ -100,7 +120,7 @@ def rotate_12_RT(d1, d2, baz, azim1, azim2):
         raise ValueError("Back Azimuth should be between 0 and 360 degree")
 
     # caculate the angle of rotation
-    angle = (baz + 180.0 - azim1) * 2 * pi / 360.
+    angle = baz + 180.0 - azim1
     r, t = rotate_certain_angle(d1, d2, angle)
 
     return r, t
@@ -136,7 +156,7 @@ def rotate_RT_12(r, t, baz, azim1, azim2):
         raise ValueError("Back Azimuth should be between 0 and 360 degree")
 
     # caculate the angle of rotation
-    angle = - (baz + 180.0 - azim) * 2 * pi / 360.
+    angle = - (baz + 180.0 - azim)
     d1, d2 = rotate_certain_angle(r, t, angle)
 
     if "right" in status:
@@ -171,8 +191,7 @@ def rotate_12_NE(d1, d2, azim1, azim2):
         raise TypeError("Component 1 and 2 have different length")
 
     # caculate the angle of rotation
-    angle = - azim1 * 2 * pi / 360.
-    n, e = rotate_certain_angle(d1, d2, angle)
+    n, e = rotate_certain_angle(d1, d2, -azim1)
 
     return n, e
 
@@ -205,8 +224,7 @@ def rotate_NE_12(n, e, azim1, azim2):
         raise TypeError("Component North and East have different length")
 
     # caculate the angle of rotation
-    angle = (azim) * 2 * pi / 360.
-    d1, d2 = rotate_certain_angle(n, e, angle)
+    d1, d2 = rotate_certain_angle(n, e, azim)
 
     if "right" in status:
         return d2, d1
@@ -216,13 +234,15 @@ def rotate_NE_12(n, e, azim1, azim2):
 
 def extract_channel_orientation_info(tr, inv):
     try:
-        sta_inv = inv[0][0]
+        nw = tr.stats.network
+        sta = tr.stats.station
         loc = tr.stats.location
         chan = tr.stats.channel
-        inv = sta_inv.select(location=loc, channel=chan)[0]
-        inc = inv.dip
-        azi = inv.azimuth
-        return inc, azi
+        chan_inv = inv.select(network=nw, station=sta, location=loc,
+                              channel=chan)[0][0][0]
+        dip = chan_inv.dip
+        azi = chan_inv.azimuth
+        return dip, azi
     except:
         return None, None
 
@@ -274,15 +294,67 @@ def rotate_12_RT_func(st, inv, method="12->RT", back_azimuth=None):
     return st
 
 
+def check_inventory_sanity(inv):
+    """
+    Check the sanity of inventory
+    Error code based on binary calculation
+    ZNE = Z * 4 + N *2 + E * 1
+    For example,
+    "4(100)" -> Z component error
+    "3(011)" -> N,E component error
+    "7(111)" -> Z,N,E component error
+    """
+    error = 0
+    inv_z = inv.select(channel="*Z")
+    for _inv in inv_z[0][0]:
+        if np.abs(_inv.dip) != 90:
+            error += 4
+        if _inv.azimuth != 0.0:
+            error += 4
+
+    inv_n = inv.select(channel="*N")
+    for _inv in inv_n[0][0]:
+        if _inv.dip != 0.0:
+            error += 2
+        if _inv.azimuth != 0.0:
+            error += 2
+
+    inv_e = inv.select(channel="*E")
+    for _inv in inv_e[0][0]:
+        if _inv.dip != 0.0:
+            error += 1
+        if _inv.azimuth != 90.0:
+            error += 1
+
+    return error
+
+
 def rotate_one_station_stream(st, event_latitude, event_longitude,
                               station_latitude=None, station_longitude=None,
-                              inventory=None, mode="ALL->RT"):
+                              inventory=None, mode="NE->RT"):
+    """
+    TODO list: check inventory sanity in the future. Make sure
+        "NE" are real NE components
+    """
 
     mode = mode.upper()
     mode_options = ["NE->RT", "ALL->RT", "12->RT", "RT->NE"]
     if mode not in mode_options:
         raise ValueError("rotate_stream mode(%s) should be within %s"
                          % (mode, mode_options))
+
+    if inventory is None and \
+            (station_latitude is None and station_longitude is None):
+        raise ValueError("You need either specify station inventory "
+                         "or station location information")
+
+    if mode in ["12->RT", "ALL->RT"] and inventory is None:
+        raise ValueError("If you want to rotate 12, you need to specify "
+                         "station inventory")
+
+    if len(sort_stream_by_station(st)) != 1:
+        raise ValueError("This method only accepts stream that contains "
+                         "one station")
 
     if station_latitude is None or station_longitude is None:
         nw = st[0].stats.network
@@ -296,17 +368,10 @@ def rotate_one_station_stream(st, event_latitude, event_longitude,
                   % (nw, station, err))
             return
 
-    _, _, baz = gps2DistAzimuth(event_latitude, event_longitude,
-                                station_latitude, station_longitude)
+    baz = calculate_baz(event_latitude, event_longitude,
+                        station_latitude, station_longitude)
 
     components = [tr.stats.channel[-1] for tr in st]
-
-    if mode in ["NE->RT", "ALL->RT"]:
-        if "N" in components and "E" in components:
-            try:
-                st.rotate(method="NE->RT", back_azimuth=baz)
-            except Exception as e:
-                print("Error rotating NE->RT:%s" % e)
 
     if mode in ["12->RT", "ALL->RT"]:
         if "1" in components and "2" in components:
@@ -314,6 +379,13 @@ def rotate_one_station_stream(st, event_latitude, event_longitude,
                 rotate_12_RT_func(st, inventory, back_azimuth=baz)
             except Exception as e:
                 print("Error rotating 12->RT:%s" % e)
+
+    if mode in ["NE->RT", "ALL->RT"]:
+        if "N" in components and "E" in components:
+            try:
+                st.rotate(method="NE->RT", back_azimuth=baz)
+            except Exception as e:
+                print("Error rotating NE->RT:%s" % e)
 
     if mode in ["RT->NE"]:
         st.rotate(method="RT->NE", back_azimuth=baz)
@@ -340,7 +412,7 @@ def sort_stream_by_station(st):
 
 
 def rotate_stream(st, event_latitude, event_longitude,
-                  inventory=None, mode="ALL->RT"):
+                  inventory, mode="ALL->RT"):
     """
     Rotate a stream to radial and transverse components based on the
     station information and event information
@@ -375,28 +447,27 @@ def rotate_stream(st, event_latitude, event_longitude,
         raise ValueError("Mode %s required inventory(stationxml) "
                          "information provided(to rotate '12')" % mode)
 
+    # the stream might contains multiple stations so each station should
+    # be rotated indepandantly
     sorted_st_dict = sort_stream_by_station(st)
 
-    if len(sorted_st_dict) == 1:
-        # if there is only one station
-        rotate_one_station_stream(st, event_latitude, event_longitude,
-                                  inventory=inventory, mode=mode)
-    else:
-        # if there are multiple stations
-        for sta_stream in sorted_st_dict.itervalues():
-            nw = sta_stream[0].stats.network
-            station = sta_stream[0].stats.station
-            loc = sta_stream[0].stats.location
+    for sta_stream in sorted_st_dict.itervalues():
+        # loop over stations
+        nw = sta_stream[0].stats.network
+        station = sta_stream[0].stats.station
+        loc = sta_stream[0].stats.location
 
-            if loc == "S3":
-                # SPECFEM TUNE
-                station_inv = inventory.select(network=nw, station=station)
-            else:
-                station_inv = inventory.select(network=nw, station=station,
-                                               location=loc)
+        if loc == "S3":
+            # SPECFEM TUNE: if the synthetic is generated by SPECFEM
+            # and the stationxml is from read data, then the location
+            # won't match between synt and staxml
+            station_inv = inventory.select(network=nw, station=station)
+        else:
+            station_inv = inventory.select(network=nw, station=station,
+                                           location=loc)
 
-            rotate_one_station_stream(sta_stream, event_latitude,
-                                      event_longitude, inventory=station_inv,
-                                      mode=mode)
-            rotated_stream += sta_stream
-            st = rotated_stream
+        rotate_one_station_stream(sta_stream, event_latitude,
+                                  event_longitude, inventory=station_inv,
+                                  mode=mode)
+        rotated_stream += sta_stream
+        st = rotated_stream
---
+++ pytomo3d/signal/tests/test_rotate.py
@@ -0,0 +1,207 @@
+import os
+import inspect
+import numpy as np
+import numpy.testing as npt
+import pytomo3d.signal.rotate as rotate
+from obspy import read, read_inventory
+
+
+def _upper_level(path, nlevel=4):
+    """
+    Go the nlevel dir up
+    """
+    for i in range(nlevel):
+        path = os.path.dirname(path)
+    return path
+
+# Most generic way to get the data folder path.
+TESTBASE_DIR = _upper_level(os.path.abspath(
+    inspect.getfile(inspect.currentframe())), 4)
+DATA_DIR = os.path.join(TESTBASE_DIR, "tests", "data")
+
+teststaxml = os.path.join(DATA_DIR, "stationxml", "IU.KBL.xml")
+testquakeml = os.path.join(DATA_DIR, "quakeml", "C201009031635A.xml")
+testobs = os.path.join(DATA_DIR, "raw", "IU.KBL.obs.mseed")
+testsyn = os.path.join(DATA_DIR, "raw", "IU.KBL.syn.mseed")
+small_mseed = os.path.join(DATA_DIR, "raw", "BW.RJOB.obs.mseed")
+
+
+def test_calculate_baz():
+    elat = 0.0
+    elon = 0.0
+    slat = 10.0
+    slon = 0.0
+    assert rotate.calculate_baz(elat, elon, slat, slon) == 180.0
+    assert rotate.calculate_baz(slat, slon, elat, elon) == 0.0
+
+    elat = 0.0
+    elon = 0.0
+    slat = 0.0
+    slon = 10.0
+    assert rotate.calculate_baz(elat, elon, slat, slon) == 270.0
+    assert rotate.calculate_baz(slat, slon, elat, elon) == 90.0
+
+
+def test_check_orthogonality():
+
+    azi1 = 1
+    azi2 = 91
+    assert rotate.check_orthogonality(azi1, azi2) == "left-hand"
+    assert rotate.check_orthogonality(azi2, azi1) == "right-hand"
+
+    azi1 = 45
+    azi2 = 315
+    assert rotate.check_orthogonality(azi1, azi2) == "right-hand"
+    assert rotate.check_orthogonality(azi2, azi1) == "left-hand"
+
+    azi1 = 46
+    azi2 = 137
+    assert not rotate.check_orthogonality(azi1, azi2)
+    assert not rotate.check_orthogonality(azi2, azi1)
+
+    azi1 = 46
+    azi2 = 314
+    assert not rotate.check_orthogonality(azi1, azi2)
+    assert not rotate.check_orthogonality(azi2, azi1)
+
+
+def test_rotate_certain_angle():
+
+    d1 = np.array([1.0, 0.0])
+    d2 = np.array([0.0, 1.0])
+
+    dnew1, dnew2 = rotate.rotate_certain_angle(d1, d2, 30.0)
+
+    dnew1_true = np.array([np.sqrt(3)/2.0, 0.5])
+    dnew2_true = np.array([-0.5, np.sqrt(3)/2.0])
+    npt.assert_almost_equal(dnew1, dnew1_true)
+    npt.assert_almost_equal(dnew2, dnew2_true)
+
+
+def test_rotate_12_NE():
+
+    d1 = np.array([1.0, 0.0])
+    d2 = np.array([0.0, 1.0])
+
+    n, e = rotate.rotate_12_NE(d1, d2, 30, 120)
+
+    n_true = np.array([np.sqrt(3)/2.0, -0.5])
+    e_true = np.array([0.5, np.sqrt(3)/2.0])
+    npt.assert_almost_equal(n, n_true)
+    npt.assert_almost_equal(e, e_true)
+
+
+def test_rotate_NE_12():
+
+    n = np.array([1.0, 0.0])
+    e = np.array([0.0, 1.0])
+
+    dnew1, dnew2 = rotate.rotate_NE_12(n, e, 30, 120)
+
+    assert rotate.check_orthogonality(30, 120) == "left-hand"
+
+    dnew1_true = np.array([np.sqrt(3)/2.0, 0.5])
+    dnew2_true = np.array([-0.5, np.sqrt(3)/2.0])
+    npt.assert_almost_equal(dnew1, dnew1_true)
+    npt.assert_almost_equal(dnew2, dnew2_true)
+
+
+def test_rotate_12_RT():
+
+    d1 = np.array([1.0, 0.0])
+    d2 = np.array([0.0, 1.0])
+    azi1 = 30
+    azi2 = 120
+    baz = 240
+
+    r, t = rotate.rotate_12_RT(d1, d2, baz, azi1, azi2)
+
+    n, e = rotate.rotate_12_NE(d1, d2, azi1, azi2)
+    r_true, t_true = rotate.rotate_NE_12(n, e, baz-180, baz-90)
+
+    npt.assert_almost_equal(r, r_true)
+    npt.assert_almost_equal(t, t_true)
+
+
+def test_rotate_RT_12():
+
+    r = np.array([1.0, 0.0])
+    t = np.array([0.0, 1.0])
+    azi1 = 30
+    azi2 = 120
+    baz = 240
+
+    d1, d2 = rotate.rotate_RT_12(r, t, baz, azi1, azi2)
+
+    n, e = rotate.rotate_12_NE(r, t, baz-180, baz-90)
+    d1_true, d2_true = rotate.rotate_NE_12(n, e, azi1, azi2)
+
+
+def test_extract_channel_orientation_info():
+    st = read(testobs)
+    inv = read_inventory(teststaxml)
+
+    tr_z = st.select(channel="*Z")[0]
+    dip, azi = rotate.extract_channel_orientation_info(tr_z, inv)
+    assert dip == -90.0
+    assert azi == 0.0
+
+    tr_e = st.select(channel="*E")[0]
+    dip, azi = rotate.extract_channel_orientation_info(tr_e, inv)
+    assert dip == 0.0
+    assert azi == 100.0
+
+    tr_n = st.select(channel="*N")[0]
+    dip, azi = rotate.extract_channel_orientation_info(tr_n, inv)
+    assert dip == 0.0
+    assert azi == 10.0
+
+
+def test_check_inventory_sanity():
+    inv = read_inventory(teststaxml)
+    error = rotate.check_inventory_sanity(inv)
+    assert error == 3
+
+
+def sort_stream_by_station():
+
+    st = read(small_mseed)
+    st += read(testobs)
+    st += read(testsyn)
+
+    assert len(rotate.sort_stream_by_station) == 3
+
+
+def test_rotate_one_station_stream():
+
+    obs = read(testobs)
+    syn = read(testsyn)
+    inv = read_inventory(teststaxml)
+
+    obs_r1 = obs.copy()
+    rotate.rotate_one_station_stream(
+        obs_r1, 0.0, 0.0, inventory=inv, mode="NE->RT")
+    obs_r2 = obs.copy()
+    rotate.rotate_one_station_stream(
+        obs_r2, 0.0, 0.0, station_latitude=34.5408,
+        station_longitude=69.0432, mode="NE->RT")
+    assert obs_r1 == obs_r2
+
+    syn_r1 = syn.copy()
+    rotate.rotate_one_station_stream(
+        syn_r1, 0.0, 0.0, inventory=inv, mode="NE->RT")
+    syn_r2 = syn.copy()
+    rotate.rotate_one_station_stream(
+        syn_r2, 0.0, 0.0, station_latitude=34.5408,
+        station_longitude=69.0432, mode="NE->RT")
+    assert syn_r1 == syn_r2
+
+
+def test_rotate_stream():
+
+    obs = read(testobs)
+    syn = read(testsyn)
+    inv = read_inventory(teststaxml)
+
+    rotate.rotate_stream(obs, 0.0, 0.0, inv, mode="NE->RT")
+    rotate.rotate_stream(syn, 0.0, 0.0, inv, mode="NE->RT")
