95052971148128ac3cbe9d06e6f22165d63df201
lei <lei@princeton.edu>
2016-11-28 17:32:29 -0500

    add tests

--- pytomo3d/window/tests/test_window_utils.py
+++ pytomo3d/window/tests/test_window_utils.py
@@ -1,14 +1,254 @@
+import os
+import pytomo3d.window.utils as wu
+from pytomo3d.utils.io import load_json
+
+
+def test_sort_windows_on_channel_and_location():
+    sta_win = {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+               "II.AAK.10.BHT": [1], "II.AAK.00.BHZ": [1, 2, 3, 4],
+               "II.AAK..BHZ": [1, 2, 3],
+               "II.AAK..EHZ": [1, 2, 3, 4, 5, 6, 7, 8],
+               "II.AAK..EHR": [1, 2, 3, 4],
+               "II.AAK.10.EHZ": [1, 2],
+               "II.AAK..LHZ": [1, 2, 3, 4]}
+    results = wu.sort_windows_on_channel_and_location(sta_win)
+    _true = {
+        'BH': {
+            '10': {'nwins': 6, 'traces': ['II.AAK.10.BHR', 'II.AAK.10.BHT',
+                                          'II.AAK.10.BHZ']},
+            '00': {'nwins': 4, 'traces': ['II.AAK.00.BHZ']},
+            '': {'nwins': 3, 'traces': ['II.AAK..BHZ']}},
+        'EH': {
+            '': {'nwins': 12, 'traces': ['II.AAK..EHR', 'II.AAK..EHZ']},
+            '10': {'nwins': 2, 'traces': ['II.AAK.10.EHZ']}},
+        'LH': {
+            '': {'nwins': 4, 'traces': ["II.AAK..LHZ"]}}}
+    assert results == _true
+
+
+def test_sort_windows_on_channel_and_location_2():
+    sta_win = {"II.AAK.10.BHZ": [], "II.AAK.00.BHZ": [1, 2, 3]}
+    results = wu.sort_windows_on_channel_and_location(sta_win)
+    _true = {'BH': {'10': {'nwins': 0, 'traces': ['II.AAK.10.BHZ']},
+                    '00': {'nwins': 3, 'traces': ['II.AAK.00.BHZ']}}}
+    assert results == _true
+
+    sta_win = {}
+    results = wu.sort_windows_on_channel_and_location(sta_win)
+    assert results == {}
+
+
+def test_pick_location_with_more_windows():
+    input = {
+        "BH": {
+            '': {'nwins': 6, 'traces': ["II.AAK..BHZ", "II.AAK..BHR"]},
+            '10': {'nwins': 10, 'traces': ["II.AAK..BHR"]}},
+        "LH": {
+            '00': {'nwins': 3, 'traces': ["II.AAK..LHZ", "II.AAK..LHR",
+                                          "II.AAK.LHT"]},
+            '20': {'nwins': 2, 'traces': ["II.AAK.20.LHZ"]}},
+        "EH": {
+            '': {'nwins': 10, 'traces': ["II.AAK..LHR"]}}
+    }
+    choosen = wu.pick_location_with_more_windows(input)
+    assert choosen == {"BH": '10', "EH": '', "LH": "00"}
+
+
+def test_pick_location_with_more_windows_2():
+    input = {
+        "BH": {
+            '': {'nwins': 0, 'traces': ["II.AAK..BHZ", "II.AAK..BHR"]}},
+        "LH": {
+            '00': {'nwins': 3, 'traces': ["II.AAK..LHZ", "II.AAK..LHR",
+                                          "II.AAK.LHT"]},
+            '20': {'nwins': 2, 'traces': ["II.AAK.20.LHZ"]}}
+    }
+    choosen = wu.pick_location_with_more_windows(input)
+    _true = {'BH': '', 'LH': '00'}
+    assert choosen == _true
+
+
 def test_merge_instruments_windows():
-    pass
+    sta_win = {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+               "II.AAK.10.BHT": [1], "II.AAK.00.BHZ": [1, 2, 3, 4],
+               "II.AAK..BHZ": [1, 2, 3],
+               "II.AAK..EHZ": [1, 2, 3, 4, 5, 6, 7, 8],
+               "II.AAK..EHR": [1, 2, 3, 4],
+               "II.AAK.10.EHZ": [1, 2],
+               "II.AAK..LHZ": [1, 2, 3, 4]}
+
+    choosen_wins = wu.merge_instruments_window(sta_win)
+    _true = {
+        'II.AAK.10.BHZ': [1, 2, 3], 'II.AAK.10.BHT': [1],
+        'II.AAK.10.BHR': [1, 2],
+        'II.AAK..EHR': [1, 2, 3, 4], 'II.AAK..EHZ': [1, 2, 3, 4, 5, 6, 7, 8],
+        'II.AAK..LHZ': [1, 2, 3, 4]}
+    assert choosen_wins == _true
+
+
+def test_sort_windows_on_channel():
+    sta_win = {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+               "II.AAK.10.BHT": [1], "II.AAK.00.BHZ": [1, 2, 3, 4],
+               "II.AAK..BHZ": [1, 2, 3],
+               "II.AAK..EHZ": [1, 2, 3, 4, 5, 6, 7, 8],
+               "II.AAK..EHR": [1, 2, 3, 4],
+               "II.AAK.10.EHZ": [1, 2],
+               "II.AAK..LHZ": [1, 2, 3, 4]}
+    sort_dict = wu.sort_windows_on_channel(sta_win)
+    _true = {
+        'BH': {'nwins': 13, 'traces': ['II.AAK.10.BHR', 'II.AAK..BHZ',
+                                       'II.AAK.10.BHT', 'II.AAK.10.BHZ',
+                                       'II.AAK.00.BHZ']},
+        'EH': {'nwins': 14, 'traces': ['II.AAK..EHR', 'II.AAK.10.EHZ',
+                                       'II.AAK..EHZ']},
+        'LH': {'nwins': 4, 'traces': ['II.AAK..LHZ']}}
+    assert sort_dict == _true
+
+
+def test_sort_windows_on_channel_2():
+    sta_win = {"II.AAK.10.BHZ": [], "II.AAK.10.BHR": [1, 2, 3, 4]}
+    sort_dict = wu.sort_windows_on_channel(sta_win)
+    _true = {
+        "BH": {"nwins": 4, "traces": ["II.AAK.10.BHZ", "II.AAK.10.BHR"]}
+    }
+    assert sort_dict == _true
+
+
+def test_pick_channel_with_more_windows():
+    input = {
+        'BH': {'nwins': 13, 'traces': ['II.AAK.10.BHR', 'II.AAK..BHZ',
+                                       'II.AAK.10.BHT', 'II.AAK.10.BHZ',
+                                       'II.AAK.00.BHZ']},
+        'EH': {'nwins': 10, 'traces': ['II.AAK..EHR', 'II.AAK.10.EHZ',
+                                       'II.AAK..EHZ']},
+        'LH': {'nwins': 4, 'traces': ['II.AAK..LHZ']}}
+
+    results = wu.pick_channel_with_more_windows(input)
+    assert results == "BH"
+
+    input = {'BH': {'nwins': 0, 'traces': ['II.AAK.00.BHR']}}
+    results = wu.pick_channel_with_more_windows(input)
+    assert results == "BH"
+
+    input = {}
+    results = wu.pick_channel_with_more_windows(input)
+    assert results is None
 
 
 def test_merge_channels_window():
-    pass
+    sta_win = {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+               "II.AAK.10.BHT": [1], "II.AAK.00.BHZ": [1, 2, 3, 4],
+               "II.AAK..BHZ": [1, 2, 3],
+               "II.AAK..EHZ": [1, 2, 3, 4, 5, 6, 7, 8],
+               "II.AAK..EHR": [1, 2, 3, 4],
+               "II.AAK.10.EHZ": [1, 2],
+               "II.AAK..LHZ": [1, 2, 3, 4]}
+
+    results = wu.merge_channels_window(sta_win)
+    _true = {'II.AAK..EHZ': [1, 2, 3, 4, 5, 6, 7, 8],
+             'II.AAK..EHR': [1, 2, 3, 4], 'II.AAK.10.EHZ': [1, 2]}
+    assert results == _true
 
 
 def test_merge_station_windows():
-    pass
+    sta_win = {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+               "II.AAK.10.BHT": [1], "II.AAK.00.BHZ": [1, 2, 3, 4],
+               "II.AAK..BHZ": [1, 2, 3],
+               "II.AAK..EHZ": [1, 2, 3, 4, 5, 6, 7, 8],
+               "II.AAK..EHR": [1, 2, 3, 4],
+               "II.AAK.10.EHZ": [1, 2],
+               "II.AAK..LHZ": [1, 2, 3, 4]}
+
+    results = wu.merge_station_windows(sta_win)
+    _true = {"II.AAK..EHZ": [1, 2, 3, 4, 5, 6, 7, 8],
+             "II.AAK..EHR": [1, 2, 3, 4]}
+    assert results == _true
+
+    sta_win = {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+               "II.AAK.10.BHT": [1], "II.AAK.00.BHZ": [1, 2, 3, 4],
+               "II.AAK..BHZ": [1, 2, 3],
+               "II.AAK..EHZ": [1, 2],
+               "II.AAK..EHR": [1, 2, 3, 4],
+               "II.AAK.10.EHZ": [1, 2],
+               "II.AAK..LHZ": [1, 2, 3, 4]}
+
+    results = wu.merge_station_windows(sta_win)
+    _true = {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+             "II.AAK.10.BHT": [1]}
+    assert results == _true
 
 
-def test_stats_all_windows():
-    pass
+def get_sample_windows():
+    AAK_win = {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+               "II.AAK.10.BHT": [1], "II.AAK.00.BHZ": [1, 2, 3, 4],
+               "II.AAK..BHZ": [1, 2, 3],
+               "II.AAK..EHZ": [1, 2],
+               "II.AAK..EHR": [1, 2, 3, 4],
+               "II.AAK.10.EHZ": [1, 2],
+               "II.AAK..LHZ": [1, 2, 3, 4]}
+    BBK_win = {"II.BBK..BHZ": [], "II.BBK..BHR": [1], "II.BBK..BHT": [1],
+               "II.BBK.10.BHZ": [1, 2], "II.BBK.10.BHR": [],
+               "II.BBK.10.BHT": [1],
+               "II.BBK..LHZ": [1, 2, 3, 4], "II.BBK..LHR": [],
+               "II.BBK..LHT": [1, 2, 3],
+               "II.BBK.10.LHZ": [], "II.BBK.10.LHR": [1]}
+    windows = {"II.AAK": AAK_win, "II.BBK": BBK_win}
+    return windows
+
+
+def test_merge_windows():
+    windows = get_sample_windows()
+    results = wu.merge_windows(windows)
+    _true = {"II.BBK": {"II.BBK..LHZ": [1, 2, 3, 4], "II.BBK..LHR": [],
+                        "II.BBK..LHT": [1, 2, 3]},
+             "II.AAK": {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+                        "II.AAK.10.BHT": [1]}}
+    assert results == _true
+
+
+def test_generate_log_content():
+    windows = {"II.BBK": {"II.BBK..BHZ": [1, 2, 3, 4], "II.BBK..BHR": [],
+                          "II.BBK..BHT": [1, 2, 3]},
+               "II.AAK": {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+                          "II.AAK.10.BHT": [1]},
+               "II.CCK": {}}
+    log = wu.generate_log_content(windows)
+    _true = {
+        'overall': {'windows': 13, 'stations_with_windows': 2,
+                    'traces_with_windows': 5, 'traces': 6, 'stations': 3},
+        'component': {
+            'BHR': {'windows': 2, 'traces_with_windows': 1, 'traces': 2},
+            'BHT': {'windows': 4, 'traces_with_windows': 2, 'traces': 2},
+            'BHZ': {'windows': 7, 'traces_with_windows': 2, 'traces': 2}}}
+
+    assert log == _true
+
+
+def test_stats_all_windows(tmpdir):
+    windows = {"II.BBK": {"II.BBK..BHZ": [1, 2, 3, 4], "II.BBK..BHR": [],
+                          "II.BBK..BHT": [1, 2, 3]},
+               "II.AAK": {"II.AAK.10.BHZ": [1, 2, 3], "II.AAK.10.BHR": [1, 2],
+                          "II.AAK.10.BHT": [1]},
+               "II.CCK": {}}
+
+    outputfile = os.path.join(str(tmpdir), "windows.log.json")
+    wu.stats_all_windows(windows, "proc_obsd_17_40", "proc_synt_17_40",
+                         True, outputfile)
+
+    log = load_json(outputfile)
+    _true = {
+        "component": {
+            "BHR": {
+                "traces": 2, "traces_with_windows": 1, "windows": 2},
+            "BHT": {
+                "traces": 2, "traces_with_windows": 2, "windows": 4},
+            "BHZ": {
+                "traces": 2, "traces_with_windows": 2, "windows": 7}},
+        "overall": {
+            "stations": 3, "stations_with_windows": 2, "traces": 6,
+            "traces_with_windows": 5, "windows": 13},
+        "instrument_merge_flag": True,
+        "obsd_tag": "proc_obsd_17_40",
+        "synt_tag": "proc_synt_17_40"
+    }
+    assert log == _true
--- pytomo3d/window/utils.py
+++ pytomo3d/window/utils.py
@@ -1,19 +1,18 @@
 """
 # functions mainly for pypaw.
 """
-import json
 import numpy as np
+from pytomo3d.utils.io import dump_json
 
 
-def merge_instruments_window(sta_win):
+def sort_windows_on_channel_and_location(sta_win):
     """
-    Merge windows from the same channel, for example, if
-    there are windows from 00.BHZ and 10.BHZ, keep only one
-    with the most windows
-    """
-    if len(sta_win) == 0:
-        return sta_win
+    functions for merge_instruments_window. the windows
+    will be sorted based on [chan][location]
 
+    :param sta_win:
+    :return:
+    """
     sort_dict = {}
     for trace_id, trace_win in sta_win.iteritems():
         chan = trace_id.split('.')[-1][0:2]
@@ -25,38 +24,64 @@ def merge_instruments_window(sta_win):
         sort_dict[chan][loc]["traces"].append(trace_id)
         sort_dict[chan][loc]["nwins"] += len(trace_win)
 
-    choosen_wins = {}
+    # sort to trace names in order
     for chan, chan_info in sort_dict.iteritems():
-        if len(chan_info.keys()) == 1:
-            choosen_loc = chan_info.keys()[0]
-        else:
-            _locs = []
-            _nwins = []
-            for loc, loc_info in chan_info.iteritems():
-                _locs.append(loc)
-                _nwins.append(loc_info["nwins"])
-            _max_idx = np.array(_nwins).argmax()
-            choosen_loc = _locs[_max_idx]
+        for loc, loc_info in chan_info.iteritems():
+            loc_info['traces'] = sorted(loc_info['traces'])
 
-        choosen_traces = sort_dict[chan][choosen_loc]["traces"]
-        for _trace_id in choosen_traces:
-            choosen_wins[_trace_id] = sta_win[_trace_id]
+    return sort_dict
+
+
+def pick_location_with_more_windows(sort_dict):
+    choosen = {}
+    for chan, chan_info in sort_dict.iteritems():
+        if len(chan_info.keys()) == 0:
+            continue
+
+        # if multiple locations available, choose the
+        # one with most number of windows
+        _locs = []
+        _nwins = []
+        for loc, loc_info in chan_info.iteritems():
+            _locs.append(loc)
+            _nwins.append(loc_info["nwins"])
+        _max_idx = np.array(_nwins).argmax()
+        choosen[chan] = _locs[_max_idx]
+
+    return choosen
+
+
+def merge_instruments_window(sta_win):
+    """
+    Merge windows from the same channel, for example, if
+    there are windows from "00.BH*" and "10.BH*", keep only one
+    with the most windows. For example, if "00.BH*" has 10
+    windows and "10.BH*" has 20 windows, we will keep the
+    "10.BH*" since it has more windows.
+    """
+    if len(sta_win) == 0:
+        return sta_win
+
+    sort_dict = sort_windows_on_channel_and_location(sta_win)
+    choosen_locs = pick_location_with_more_windows(sort_dict)
+
+    choosen_wins = {}
+    for chan, loc in choosen_locs.iteritems():
+        trace_list = sort_dict[chan][loc]["traces"]
+        for tr_id in trace_list:
+            choosen_wins[tr_id] = sta_win[tr_id]
 
     return choosen_wins
 
 
-def merge_channels_window(sta_win):
+def sort_windows_on_channel(sta_win):
     """
-    Merge windows from different channels.
-    This step should be done after merge instruments windows
-    because after that there will only one instrument left
-    on one channel
+    Gounp windows from one station into channels and count
+    the number of windows in that channel
+    :param sta_win:
+    :return:
     """
     sort_dict = {}
-
-    if len(sta_win) == 0:
-        return sta_win
-
     for trace_id, trace_win in sta_win.iteritems():
         chan = trace_id.split(".")[-1][0:2]
         if chan not in sort_dict:
@@ -64,19 +89,36 @@ def merge_channels_window(sta_win):
         sort_dict[chan]["traces"].append(trace_id)
         sort_dict[chan]["nwins"] += len(trace_win)
 
-    choosen_wins = {}
-    if len(sort_dict.keys()) == 1:
-        choosen_chan = sort_dict.keys()[0]
-    else:
-        _chans = []
-        _nwins = []
-        for chan, chan_info in sort_dict.iteritems():
-            _chans.append(chan)
-            _nwins.append(chan_info["nwins"])
-        _max_idx = np.array(_nwins).argmax()
-        choosen_chan = _chans[_max_idx]
+    return sort_dict
 
+
+def pick_channel_with_more_windows(sort_dict):
+    max_wins = -1
+    max_chan = None
+    for chan, chaninfo in sort_dict.iteritems():
+        if chaninfo["nwins"] > max_wins:
+            max_wins = chaninfo["nwins"]
+            max_chan = chan
+    return max_chan
+
+
+def merge_channels_window(sta_win):
+    """
+    Merge windows from different channels.
+    This step should be done after merge instruments windows
+    because after that there will only one instrument left
+    on one channel.
+    For example, if we have "BH" channel with 20 windows and
+    "LH" has 10 windows, we will keep only the "BH" channel.
+    """
+    if len(sta_win) == 0:
+        return sta_win
+
+    sort_dict = sort_windows_on_channel(sta_win)
+    choosen_chan = pick_channel_with_more_windows(sort_dict)
     choosen_traces = sort_dict[choosen_chan]["traces"]
+
+    choosen_wins = {}
     for _trace_id in choosen_traces:
         choosen_wins[_trace_id] = sta_win[_trace_id]
 
@@ -85,10 +127,16 @@ def merge_channels_window(sta_win):
 
 def merge_station_windows(windows):
     """
-    Merge windows for one station group.
-    1) merge multiple instruments: keep the instrument with the most
-    number of windows
-    2) merge channels
+    Merge windows for one station.
+    For example, you may have "00.BH", "10.BH", "00.LH", "10.LH" from
+    different locations and channels. You may only want to keep one
+    at the very end. So:
+    1) select locations: keep only one location with the most
+        number of windows for one channel. For example, in "00.BH" and
+        "10.BH" we only keep "10.BH".
+    2) select channel: after last step, we keep only "00.BH" and "10.LH",
+        but ultimately we only want to keep one. So we may only choose
+        "00.BH" since it has more windows.
     """
     w = merge_instruments_window(windows)
     w = merge_channels_window(w)
@@ -97,7 +145,7 @@ def merge_station_windows(windows):
 
 def merge_windows(windows):
     """
-        Merge the windows(from one event) based on insturments
+    Merge the windows(from one event, multiple stations)
     """
     new_windows = {}
     for sta, sta_info in windows.iteritems():
@@ -108,35 +156,49 @@ def merge_windows(windows):
     return new_windows
 
 
+def generate_log_content(windows):
+    overall_log = {"stations": 0, "stations_with_windows": 0,
+                   "windows": 0, "traces": 0, "traces_with_windows": 0}
+    comp_log = {}
+    for sta_name, sta_win in windows.iteritems():
+        if sta_win is None:
+            continue
+        nwin_sta = 0
+        ntraces_with_windows = 0
+        for trace_id, trace_win in sta_win.iteritems():
+            comp = trace_id.split(".")[-1]
+            if comp not in comp_log:
+                comp_log[comp] = {
+                    "windows": 0, "traces": 0, "traces_with_windows": 0}
+            comp_log[comp]["windows"] += len(trace_win)
+            if len(trace_win) > 0:
+                comp_log[comp]["traces_with_windows"] += 1
+                ntraces_with_windows += 1
+            comp_log[comp]["traces"] += 1
+            nwin_sta += len(trace_win)
+
+        overall_log["stations"] += 1
+        overall_log["windows"] += nwin_sta
+        overall_log["traces"] += len(sta_win)
+        overall_log["traces_with_windows"] += ntraces_with_windows
+        if nwin_sta > 0:
+            overall_log["stations_with_windows"] += 1
+
+    log = {"component": comp_log, "overall": overall_log}
+    return log
+
+
 def stats_all_windows(windows, obsd_tag, synt_tag,
                       instrument_merge_flag,
                       output_file):
     """
     Generate window statistic information
     """
+    log = {"obsd_tag": obsd_tag, "synt_tag": synt_tag,
+           "instrument_merge_flag": instrument_merge_flag}
 
-    window_stats = {"obsd_tag": obsd_tag, "synt_tag": synt_tag,
-                    "instrument_merge_flag": instrument_merge_flag,
-                    "stations": 0, "stations_with_windows": 0}
-    for sta_name, sta_win in windows.iteritems():
-        if sta_win is None:
-            continue
-        nwin_sta = 0
-        for trace_id, trace_win in sta_win.iteritems():
-            comp = trace_id.split(".")[-1]
-            if comp not in window_stats:
-                window_stats[comp] = {"window": 0, "traces": 0,
-                                      "traces_with_windows": 0}
-            window_stats[comp]["window"] += len(trace_win)
-            if len(trace_win) > 0:
-                window_stats[comp]["traces_with_windows"] += 1
-            window_stats[comp]["traces"] += 1
-            nwin_sta += len(trace_win)
-
-        window_stats["stations"] += 1
-        if nwin_sta > 0:
-            window_stats["stations_with_windows"] += 1
+    window_log = generate_log_content(windows)
+    log.update(window_log)
 
     print("Windows statistic log file: %s" % output_file)
-    with open(output_file, "w") as fh:
-        json.dump(window_stats, fh, indent=2, sort_keys=True)
+    dump_json(log, output_file)
