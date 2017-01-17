6bd4e3309dfa632c6daefdee72159c32253950e4
Youyi Ruan <youyir@princeton.edu>
2017-01-17 11:21:33 -0500

    add amplitude check for all test functions

--- pytomo3d/window/tests/test_filter_windows.py
+++ pytomo3d/window/tests/test_filter_windows.py
@@ -101,15 +101,15 @@ def test_get_measurements_std():
          "Z": np.std([1, 2, -1.5, 2, -5, -0.2, 0.8, -1.6, 1.6, 0.9])}
 
     _true_dlna_mean = \
-        {"R": np.mean([1.0, -1.0, 1.0, 1.0, -0.8]),
-         "T": np.mean([0.3, 0.3, -0.7]),
+        {"R": np.mean([0.7, -0.7, 0.6, 1.0, -0.8]),
+         "T": np.mean([0.9, 0.3, -0.7]),
          "Z": np.mean([0.6, 0.4, -0.5, 1.2, -1.5, -0.2, 0.8, -0.6, 1.1, 0.9])}
     _true_dlna_stds = \
-        {"R": np.mean([1.0, -1.0, 1.0, 1.0, -0.8]),
-         "T": np.mean([0.3, 0.3, -0.7]),
-         "Z": np.mean([0.6, 0.4, -0.5, 1.2, -1.5, -0.2, 0.8, -0.6, 1.1, 0.9])}
+        {"R": np.std([0.7, -0.7, 0.6, 1.0, -0.8]),
+         "T": np.std([0.9, 0.3, -0.7]),
+         "Z": np.std([0.6, 0.4, -0.5, 1.2, -1.5, -0.2, 0.8, -0.6, 1.1, 0.9])}
 
-    for comp in means:
+    for comp in dt_means:
         npt.assert_array_almost_equal(dt_means[comp], _true_dt_mean[comp])
         npt.assert_array_almost_equal(dt_stds[comp], _true_dt_stds[comp])
         npt.assert_array_almost_equal(dlna_means[comp], _true_dlna_mean[comp])
@@ -118,36 +118,48 @@ def test_get_measurements_std():
 
 
 def test_get_user_bound():
-    info = {"tshift_acceptance_level": 3, "tshift_reference": 0}
+    info = {"tshift_acceptance_level": 3, "tshift_reference": 0,
+            "dlna_acceptance_level": 0.8, "dlna_reference": 0.0}
     v = fw.get_user_bound(info)
-    npt.assert_array_almost_equal(v, [-3, 3])
+    npt.assert_array_almost_equal(v, [-3, 3, -0.8, 0.8])
 
-    info = {"tshift_acceptance_level": 10, "tshift_reference": -1}
+    info = {"tshift_acceptance_level": 10, "tshift_reference": -1,
+            "dlna_acceptance_level": 0.6, "dlna_reference": 0.2}
     v = fw.get_user_bound(info)
-    npt.assert_array_almost_equal(v, [-11, 9])
+    npt.assert_array_almost_equal(v, [-11, 9, -0.4, 0.8])
 
 
 def test_filter_measurements_on_bounds():
-    bounds = {"R": [-1.1, 1.1], "T": [-1.1, 1.1], "Z": [-1.1, 1.1]}
+    bounds = {"R": [-1.1, 1.1, -0.8, 0.8],
+              "T": [-1.1, 1.1, -0.8, 0.8],
+              "Z": [-1.1, 1.1, -0.8, 0.8]}
 
     v, m = fw.filter_measurements_on_bounds(windows, measures, bounds)
     assert v["II.AAK"]["II.AAK..BHR"] == windows["II.AAK"]["II.AAK..BHR"]
     assert "II.AAK..BHT" not in v["II.AAK"]
     assert len(v["II.AAK"]["II.AAK..BHZ"]) == 1
     assert v["II.AAK"]["II.AAK..BHZ"] == [{"left_index": 1, "right_index": 2}]
+    assert len(v["IU.BCD"]["IU.BCD..BHZ"]) == 2
+    assert v["IU.BCD"]["IU.BCD..BHZ"] == [{"left_index": 1, "right_index": 2},
+                                          {"left_index": 2, "right_index": 3}]
 
     assert m["II.AAK"] == {
-        "II.AAK..BHR": [{"dt": 1.0, "misfit_dt": 1.0},
-                        {"dt": -1.0, "misfit_dt": 1.0}],
-        "II.AAK..BHZ": [{"dt": 1.0, "misfit_dt": 1.0}]}
+        "II.AAK..BHR": [{"dt": 1.0, "misfit_dt": 1.0,
+                         "dlna": 0.7, "misfit_dlna": 1.0},
+                        {"dt": -1.0, "misfit_dt": 1.0,
+                         "dlna": -0.7, "misfit_dlna": 1.0}],
+        "II.AAK..BHZ": [{"dt": 1.0, "misfit_dt": 1.0,
+                         "dlna": 0.6, "misfit_dlna": 1.0}]}
     assert m["II.ABKT"] == {
-       "II.ABKT..BHR": [{"dt": 1.0, "misfit_dt": 1.5}]}
+       "II.ABKT..BHR": [{"dt": 1.0, "misfit_dt": 1.5,
+                         "dlna": 0.6, "misfit_dlna": 0.5}]}
     assert m["IU.BCD"] == {
-           "IU.BCD..BHR": [{"dt": 1.0, "misfit_dt": 2.0}],
-           "IU.BCD..BHT": [{"dt": 1.0, "misfit_dt": 2.0}],
-           "IU.BCD..BHZ": [{"dt": -0.2, "misfit_dt": 2.0},
-                           {"dt": 0.8, "misfit_dt": 3.0},
-                           {"dt": 0.9, "misfit_dt": 0.4}]}
+           "IU.BCD..BHT": [{"dt": 1.0, "misfit_dt": 2.0,
+                            "dlna": 0.3, "misfit_dlna": 0.2}],
+           "IU.BCD..BHZ": [{"dt": -0.2, "misfit_dt": 2.0,
+                            "dlna": -0.2, "misfit_dlna": 2.0},
+                           {"dt": 0.8, "misfit_dt": 3.0,
+                            "dlna": 0.8, "misfit_dlna": 0.6}]}
 
     bounds = {"R": [-0.1, 0.1], "T": [-0.1, 0.1], "Z": [-0.1, 0.1]}
     v, m = fw.filter_measurements_on_bounds(windows, measures, bounds)
@@ -167,40 +179,54 @@ def test_filter_windows_on_measurements():
     measure_config = \
         {"component": {
             "R": {"tshift_reference": 0, "tshift_acceptance_level": 10,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 4.0},
             "T": {"tshift_reference": 0, "tshift_acceptance_level": 10,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 4.0},
             "Z": {"tshift_reference": 0, "tshift_acceptance_level": 10,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 4.0}}}
 
     _wins, _meas = fw.filter_windows_on_measurements(
         windows, measures, measure_config)
     assert _wins["II.AAK"] == windows["II.AAK"]
-    assert _wins["IU.BCD"] == windows["IU.BCD"]
+    assert _wins["IU.BCD"]["IU.BCD..BHR"] == windows["IU.BCD"]["IU.BCD..BHR"]
+    assert _wins["IU.BCD"]["IU.BCD..BHT"] == windows["IU.BCD"]["IU.BCD..BHT"]
     assert _wins["II.ABKT"]["II.ABKT..BHR"] == \
         windows["II.ABKT"]["II.ABKT..BHR"]
-    assert _wins["II.ABKT"]["II.ABKT..BHZ"] == \
-        windows["II.ABKT"]["II.ABKT..BHZ"]
+    assert len(_wins["IU.BCD"]["IU.BCD..BHZ"]) == 4
     assert "II.ABKT..BHT" not in _wins["II.ABKT"]
+    assert "II.ABKT..BHZ" not in _wins["II.ABKT"]
     assert_wins_and_meas_same_length(_wins, _meas)
 
     measure_config = \
         {"component": {
             "R": {"tshift_reference": 0, "tshift_acceptance_level": 10.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 1.0},
             "T": {"tshift_reference": 0, "tshift_acceptance_level": 10.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 1.0},
             "Z": {"tshift_reference": 0, "tshift_acceptance_level": 10.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 1.0}}}
 
     _wins, _meas = fw.filter_windows_on_measurements(
         windows, measures, measure_config)
-    assert _wins["II.ABKT"]["II.ABKT..BHZ"] == \
+    assert _wins["II.AAK"]["II.AAK..BHR"] == \
         [{"left_index": 1, "right_index": 2}]
-    assert _wins["IU.BCD"]["IU.BCD..BHR"] == \
-        [{"left_index": 1, "right_index": 2}]
+    assert _wins["II.AAK"]["II.AAK..BHZ"] == \
+        [{"left_index": 1, "right_index": 2},
+         {"left_index": 2, "right_index": 3},
+         {"left_index": 3, "right_index": 4}]
     assert _wins["IU.BCD"]["IU.BCD..BHT"] == \
         [{"left_index": 1, "right_index": 2}]
+    assert _wins["IU.BCD"]["IU.BCD..BHZ"] == \
+        [{"left_index": 1, "right_index": 2},
+         {"left_index": 2, "right_index": 3},
+         {"left_index": 3, "right_index": 4},
+         {"left_index": 5, "right_index": 6}]
     assert_wins_and_meas_same_length(_wins, _meas)
 
 
@@ -209,10 +235,13 @@ def test_filter_windows_on_measurements_2():
     measure_config = \
         {"component": {
             "R": {"tshift_reference": 0, "tshift_acceptance_level": 0.1,
+                  "dlna_reference": 0, "dlna_acceptance_level": 0.1,
                   "std_ratio": 1.0},
             "T": {"tshift_reference": 0, "tshift_acceptance_level": 0.1,
+                  "dlna_reference": 0, "dlna_acceptance_level": 0.1,
                   "std_ratio": 1.0},
             "Z": {"tshift_reference": 0, "tshift_acceptance_level": 0.1,
+                  "dlna_reference": 0, "dlna_acceptance_level": 0.1,
                   "std_ratio": 1.0}}
          }
 
@@ -239,10 +268,13 @@ def test_filter_windows():
     measure_config = \
         {"component": {
             "R": {"tshift_reference": 0, "tshift_acceptance_level": 6.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 3.0},
             "T": {"tshift_reference": 0, "tshift_acceptance_level": 6.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 3.0},
             "Z": {"tshift_reference": 0, "tshift_acceptance_level": 6.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 3.0}},
          "flag": True}
 
@@ -253,34 +285,39 @@ def test_filter_windows():
     assert _wins["II.AAK"] == windows["II.AAK"]
     assert _wins["II.ABKT"]["II.ABKT..BHR"] == \
         windows["II.ABKT"]["II.ABKT..BHR"]
-    assert _wins["II.ABKT"]["II.ABKT..BHZ"] == \
-        windows["II.ABKT"]["II.ABKT..BHZ"]
+    assert "II.ABKT..BHZ" not in _wins["II.ABKT"]
     assert "II.ABKT..BHT" not in _wins["II.ABKT"]
     assert "IU.BCD" not in _wins["II.ABKT"]
 
     assert _meas["II.AAK"] == {
-        "II.AAK..BHR": [{"dt": 1.0, "misfit_dt": 1.0},
-                        {"dt": -1.0, "misfit_dt": 1.0}],
-        "II.AAK..BHT": [{"dt": 1.5, "misfit_dt": 2.5}],
-        "II.AAK..BHZ": [{"dt": 1.0, "misfit_dt": 1.0},
-                        {"dt": 2.0, "misfit_dt": 4.0},
-                        {"dt": -1.5, "misfit_dt": 3.00}]}
+        "II.AAK..BHR": [{"dt": 1.0, "misfit_dt": 1.0,
+                         "dlna": 0.7, "misfit_dlna": 1.0},
+                        {"dt": -1.0, "misfit_dt": 1.0,
+                         "dlna": -0.7, "misfit_dlna": 1.0}],
+        "II.AAK..BHT": [{"dt": 1.5, "misfit_dt": 2.5,
+                         "dlna": 0.9, "misfit_dlna": 0.5}],
+        "II.AAK..BHZ": [{"dt": 1.0, "misfit_dt": 1.0,
+                         "dlna": 0.6, "misfit_dlna": 1.0},
+                        {"dt": 2.0, "misfit_dt": 4.0,
+                         "dlna": 0.4, "misfit_dlna": 0.6},
+                        {"dt": -1.5, "misfit_dt": 3.00,
+                         "dlna": -0.5, "misfit_dlna": 1.0}]}
 
     assert _meas["II.ABKT"] == {
-        "II.ABKT..BHR": [{"dt": 1.0, "misfit_dt": 1.5}],
-        "II.ABKT..BHZ": [{"dt": 2.0, "misfit_dt": 4.0},
-                         {"dt": -5.0, "misfit_dt": 16.0}]}
+        "II.ABKT..BHR": [{"dt": 1.0, "misfit_dt": 1.5,
+                          "dlna": 0.6, "misfit_dlna": 0.5}]}
     assert_wins_and_meas_same_length(_wins, _meas)
 
+    print(log)
     _true_log = \
         {'sensor': {
-            'window_rejection_percentage': 50.0, 'nwindows_old': 18,
-            'channel_rejection_percentage': 37.5, 'nwindows_new': 9,
+            'window_rejection_percentage': '50.00', 'nwindows_old': 18,
+            'channel_rejection_percentage': '37.50', 'nwindows_new': 9,
             'nchannels_new': 5, 'nchannels_old': 8},
          'measurement': {
-             'window_rejection_percentage': 0.0, 'nwindows_old': 9,
-             'channel_rejection_percentage': 0.0, 'nwindows_new': 9,
-             'nchannels_new': 5, 'nchannels_old': 5}}
+             'window_rejection_percentage': '22.22', 'nwindows_old': 9,
+             'channel_rejection_percentage': '20.00', 'nwindows_new': 7,
+             'nchannels_new': 4, 'nchannels_old': 5}}
 
     assert log == _true_log
 
@@ -290,10 +327,13 @@ def test_filter_windows_2():
     measure_config = \
         {"component": {
             "R": {"tshift_reference": 0, "tshift_acceptance_level": 0.1,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 3.0},
             "T": {"tshift_reference": 0, "tshift_acceptance_level": 0.1,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 3.0},
             "Z": {"tshift_reference": 0, "tshift_acceptance_level": 0.1,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 3.0}},
          "flag": True}
 
@@ -308,10 +348,13 @@ def test_filter_windows_2():
     measure_config = \
         {"component": {
             "R": {"tshift_reference": 0, "tshift_acceptance_level": 10.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 0.01},
             "T": {"tshift_reference": 0, "tshift_acceptance_level": 10.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 0.01},
             "Z": {"tshift_reference": 0, "tshift_acceptance_level": 10.0,
+                  "dlna_reference": 0, "dlna_acceptance_level": 1.0,
                   "std_ratio": 0.01}},
          "flag": True}
     config = {"sensor": sensor_config, "measurement": measure_config}
